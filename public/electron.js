80811b7282074e81359c38af88a1e6d00b20411d
brandious <adnanfacic96@gmail.com>
2021-04-21 18:26:48 +0200

    background set

--- public/electron.js
+++ public/electron.js
@@ -1,17 +1,16 @@
 const electron = require("electron");
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
-// const path = require("path");
-// const isDev = require("electron-is-dev");
 
 
 let mainWindow;
 
 function createWindow() {
-  mainWindow = new BrowserWindow({ width: 1920, height: 1080, frame: false });
+  mainWindow = new BrowserWindow({ width: 1920, height: 1080, frame: false, transparent: true});
   mainWindow.loadURL(
      `https://ap-signage.web.app/totem3`
   );
+  mainWindow.setBackgroundColor('#000000')
   mainWindow.maximize()
   mainWindow.setFullScreen(true)
   mainWindow.on("closed", () => (mainWindow = null));
